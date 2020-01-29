7dd3c6f5069208d666fb22d14a2d08781d436d69
William Beard <52893758+wbeard01@users.noreply.github.com>
2020-01-29 16:27:12 -0500

    Create OrbitalTeleOp.java
    This is our initial commit for our second generation TeleOp. This version includes basic mecanum movement so we can test our upgraded drivetrain.

---
+++ OrbitalTeleOp.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "OrbitalTeleOp", group = "TeleOp")
+public class OrbitalTeleOp extends LinearOpMode {
+
+    private DcMotor lb, lf, rb, rf;
+
+    @Override
+    public void runOpMode() {
+
+        lb = hardwareMap.get(DcMotor.class, "lb");
+        lf = hardwareMap.get(DcMotor.class, "lf");
+        rb = hardwareMap.get(DcMotor.class, "rb");
+        rf = hardwareMap.get(DcMotor.class, "rf");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            double r = Math.hypot(gamepad1.left_stick_x - gamepad1.right_stick_x, gamepad1.left_stick_y - gamepad1.right_stick_y);
+            double robotAngle = Math.atan2(gamepad1.left_stick_y - gamepad1.right_stick_y, -gamepad1.left_stick_x + gamepad1.right_stick_x) - Math.PI / 4;
+            double rightX = gamepad1.right_trigger - gamepad1.left_trigger;
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
+            lf.setPower(v1);
+            rf.setPower(-v2);
+            lb.setPower(v3);
+            rb.setPower(-v4);
+
+        }
+
+    }
+
+}

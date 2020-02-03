336a5169539eb64199195f2d0cb3e9ea7d067499
William Beard <52893758+wbeard01@users.noreply.github.com>
2020-02-03 14:53:48 -0500

    Update OrbitalAutoTest.java
    Extended forward movement to have all parameters and added a second variable edge for smoother motion control.

--- OrbitalAutoTest.java
+++ OrbitalAutoTest.java
@@ -17,7 +17,9 @@ public class OrbitalAutoTest extends LinearOpMode {
 
         //All right, let's see what we can do!
         setStopMode(DcMotor.ZeroPowerBehavior.BRAKE);
-        forward(1000);
+        forward(1000, 0.1, 0.5, 0.65, 0.15, 0.333, 0.333);
+
+        while (opModeIsActive()) {}
     }
 
     public void setupHardware() {
@@ -60,7 +62,7 @@ public class OrbitalAutoTest extends LinearOpMode {
 
     }
 
-    public void forward(int amt) {
+    public void forward(int amt, double edge1, double edge2, double max, double min, double p1, double p2) {
 
         //Reset all target positions... +/- varies by side
         rf.setTargetPosition(rf.getCurrentPosition() + amt);
@@ -71,16 +73,20 @@ public class OrbitalAutoTest extends LinearOpMode {
         //Set run mode
         setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        //Turn on all the motors
-        double prop = (amt - (rf.getTargetPosition() - rf.getCurrentPosition())) / (double) amt;
-        double adjPower = getPower(prop, 0.25, 1);
-        rf.setPower(adjPower);
-        rb.setPower(adjPower);
-        lf.setPower(adjPower);
-        lb.setPower(adjPower);
-
         //Stall until we're done moving or the OpMode is no longer active
-        while ((rf.isBusy() && rb.isBusy() && lf.isBusy() && lb.isBusy()) && opModeIsActive()) {}
+        while ((rf.isBusy() && rb.isBusy() && lf.isBusy() && lb.isBusy()) && opModeIsActive()) {
+
+            //Turn on all the motors
+            double prop = (amt - (rf.getTargetPosition() - rf.getCurrentPosition())) / (double) amt;
+            telemetry.addData("P", prop);
+            telemetry.update();
+            double adjPower = getPower(prop, edge1, edge2, max, min, p1, p2);
+            rf.setPower(adjPower);
+            rb.setPower(adjPower);
+            lf.setPower(adjPower);
+            lb.setPower(adjPower);
+
+        }
 
         //Turn off!
         stopRobot();
@@ -90,40 +96,41 @@ public class OrbitalAutoTest extends LinearOpMode {
 
     }
 
-    public static double getPower(double amtDone, double edge, double max) {
+    public double getPower(double amtDone, double edge1, double edge2, double max, double min, double p1, double p2) {
 
         //Determine power based on an adjustable curve metric inspired by the Normal Distribution
-        if (amtDone >= edge && amtDone <= 1 - edge) {
+        if (amtDone >= edge1 && amtDone <= 1 - edge2) {
 
             //We've accelerated and are in the middle of our motion, so we're at max power.
             return max;
 
         } else {
 
-            if (amtDone > 1 - edge) {
+            if (amtDone > 1 - edge2) {
 
-                //It's symmetric
+                //Last edge... what's our power?
                 amtDone = 1 - amtDone;
+                double amtNormDone = amtDone / edge2;
+                return min + evaluateNormal(1, p2, amtNormDone, max - min);
+
+            } else {
+
+                //How much of the way through are we, and what power should we be on?
+                double amtNormDone = amtDone / edge1;
+                return min + evaluateNormal(1, p1, amtNormDone, max - min);
 
             }
-            //How much of the way through are we, and what power should we be on?
-            double amtNormDone = amtDone / edge;
-            return evaluateNormal(1, 0.333, amtNormDone, max);
 
         }
 
     }
 
-
-
-    public static double evaluateNormal(double mu, double sigma, double x, double max) {
+    public double evaluateNormal(double mu, double sigma, double x, double max) {
 
         //Return an adjusted Normal Distribution value such that the maximum possible value is "max"
         double exponent = -Math.pow(x - mu, 2) / (2 * Math.pow(sigma, 2));
         return max * Math.pow(Math.E, exponent);
 
     }
-
-
 
 }
